8005426c09f47469398e973cefb77167a8055141
Muhammad Waqar <muhammadwaqar.business@gmail.com>
2024-04-11 17:38:41 +0500

    simple app

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  distDir: "out",
+  trailingSlash: true,
   reactStrictMode: true,
+  staticPageGenerationTimeout: 1000,
 };
 
 export default nextConfig;
